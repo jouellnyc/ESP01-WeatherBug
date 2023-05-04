7ea1144d5e6076e8e81b0e6cc636b8babe11b7a7
Sourcery AI <>
2023-05-04 22:41:16 +0000

    'Refactored by Sourcery'

--- weatherapp/weather_runner.py
+++ weatherapp/weather_runner.py
@@ -10,6 +10,7 @@ def fill_show():
     myoled.oled.show()
 
 """ NETWORK SETUP """
+
 from . import network_setup
 time.sleep(2)
 
@@ -44,15 +45,17 @@ while True:
     myoled.oled.show()
 
     try:
-        yr, mt, dy, hr, mn, s1, s2, s3 =  time.localtime() 
+        yr, mt, dy, hr, mn, s1, s2, s3 =  time.localtime()
         if hr < 10:
-            hr = '0' + str(hr)
+            hr = f'0{str(hr)}'
         if mn < 10:
-            mn = '0' + str(mn)
+            mn = f'0{str(mn)}'
         short_yr = str(yr)[2:]
-        myoled.oled.text(str(mt) + '-' + str(dy) + '-' +  str(short_yr) + ' ' + str(hr) + ':' + str(mn),  0, 0)
+        myoled.oled.text(
+            f'{str(mt)}-{str(dy)}-{str(short_yr)} {str(hr)}:{str(mn)}', 0, 0
+        )
         myoled.oled.text(city, 0, 17)
-        myoled.oled.text(str(temp) + "F", 0, 34)
+        myoled.oled.text(f"{str(temp)}F", 0, 34)
         myoled.oled.text(info,0, 51)
         myoled.oled.show()
     except Exception as e:
